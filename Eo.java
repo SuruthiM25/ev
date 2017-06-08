e66924cf321201fcdd68b5ae3e8e97a33e490e36
SuruthiM25 <msuruthi.77@gmail.com>
2017-06-08 18:24:35 +0530

    Create Eo.java

---
+++ Eo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Eo
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("Even number");
+}
+else
+{
+System.out.println("Odd number");
+}
+}
+}
